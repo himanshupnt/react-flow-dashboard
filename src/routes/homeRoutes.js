77fbb4d1011a548fb72c0361b02e115972d51981
Himanshu Pant <himanshu.pn3@gmail.com>
2018-08-29 11:17:22 -0700

    Adding routes

---
+++ src/routes/homeRoutes.js
@@ -0,0 +1,19 @@
+// @flow
+import type { ComponentType } from 'react';
+
+import System from '../components/System';
+import Dashboard from '../components/Dashboard';
+
+type _Route = {
+  path: string,
+  component: ComponentType<Object>,
+  key: string,
+  exact: boolean,
+};
+
+const homeRoutes: Array<_Route> = [
+  { path: '/home/dashboard', component: Dashboard, key: 'dash1', exact: true },
+  { path: '/home/system', component: System, key: 'sys1', exact: true },
+];
+
+export default homeRoutes;
---
+++ src/routes/index.js
@@ -0,0 +1,20 @@
+// @flow
+import type { ComponentType } from 'react';
+
+import FileUploader from '../components/FileUploader';
+import DashboardLayout from '../layouts/DashboardLayout';
+import NotFound from '../components/NotFound';
+
+type _Route = {
+  path: string,
+  component: ComponentType<Object>,
+  exact: boolean,
+};
+
+const indexRoutes: Array<_Route> = [
+  { path: '/upload', component: FileUploader, exact: true, key: 'uploadRt' },
+  { path: '/404', component: NotFound, exact: true, key: '404Rt' },
+  { path: '/home', component: DashboardLayout, exact: false, key: 'homeRt' },
+];
+
+export default indexRoutes;
