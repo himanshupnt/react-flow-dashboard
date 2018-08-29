0ff2810d15d3d99af5f549e95d0cb7ad9d2a005d
Himanshu Pant <himanshu.pn3@gmail.com>
2018-08-29 11:20:00 -0700

    Adding theme

---
+++ src/hoc/withTheme.js
@@ -0,0 +1,44 @@
+// @flow
+import * as React from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      light: purple[300],
+      main: purple[500],
+      dark: purple[700],
+    },
+    secondary: {
+      light: green[300],
+      main: green[500],
+      dark: green[700],
+    },
+  },
+});
+
+type Props = {
+  title: string,
+};
+
+const withTheme = (Component: React.ComponentType<Props>) => {
+  const WithTheme = (props: Props) => {
+    // MuiThemeProvider makes the theme available down the React tree
+    // thanks to React context.
+    return (
+      <MuiThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...props} />
+      </MuiThemeProvider>
+    );
+  };
+
+  return WithTheme;
+};
+
+export default withTheme;
